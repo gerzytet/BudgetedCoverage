c85184db6dca83a1b59ebf4a4d1a02d87f89fd16
SuperLan11 <92647630+SuperLan11@users.noreply.github.com>
2023-02-23 13:43:00 -0500

    Brackets

--- sensors.cpp
+++ sensors.cpp
@@ -4,21 +4,26 @@
 
 using namespace std;
 
-struct Sensor {
+struct Sensor 
+{
     double x, y;
     
-    Sensor(double x, double y): x(x), y(y) {
+    Sensor(double x, double y): x(x), y(y) 
+    {
         
     }
 };
 
 minstd_rand randomGenerator(1);
 
-int randint(int a, int b) {
+int randint(int a, int b) 
+{
     uniform_int_distribution<int> distribution(a, b);
     return distribution(randomGenerator);
 }
-double randfloat(double a, double b) {
+
+double randfloat(double a, double b) 
+{
     uniform_real_distribution<double> distribution(a, b);
     return distribution(randomGenerator);
 }
@@ -26,12 +31,15 @@ double randfloat(double a, double b) {
 vector<Sensor> sensors;
 const int NUM_POINTS = 40;
 
-int main() {
-    for (int i = 0; i < NUM_POINTS; i++) {
+int main() 
+{
+    for (int i = 0; i < NUM_POINTS; i++) 
+    {
         sensors.push_back(Point(randfloat(-5, 5), randfloat(-5, 5)));
     }
     
-    for (Sensor &s : sensors) {
+    for (Sensor &s : sensors)
+    {
         cout << setprecision(5) << '(' << s.x << ", " << s.y << ")\n";
     }
 }
