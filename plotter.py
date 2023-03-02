910ca43f7f200503ca13434d60c48b02b0736155
White <cwhite1412@floridapoly.edu>
2023-03-02 13:56:33 -0500

    Fix the plotter to display the circles the right size

--- plotter.py
+++ plotter.py
@@ -1,5 +1,6 @@
 import random
 import matplotlib.pyplot as plt
+import math
 
 with open('output.txt') as data:
     lines = list(map(str.strip, data.read().split('\n')))
@@ -44,11 +45,19 @@ while not lines[index].isnumeric():
 R = int(lines[index])
 
 colors = [(random.random() / 5) + 0.4 for i in range(len(chosen))]
-area = R ** 2
+
+fig = plt.figure()
+fig.set_figwidth(5)
+fig.set_figheight(5)
+inch_per_unit = (math.pi) / 100
+dots_per_inch = fig.dpi
+dots_per_unit = inch_per_unit * dots_per_inch
+area = math.pi * (R * dots_per_unit) ** 2
 print(area)
 
 plt.ylim(0, 100)
 plt.xlim(0, 100)
 plt.scatter(chosen_x, chosen_y, marker='o', c=colors, s=[area]*len(chosen), alpha = 0.5)
+#plt.scatter([20], [20], marker='o', s=[area], alpha = 0.5)
 plt.scatter(x_data, y_data, marker='X')
 plt.show()
