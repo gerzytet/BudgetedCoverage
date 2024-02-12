e8e2d5607727e85bbaba0c2e8a96e5e50ca761bf
Craig White <gerzytet@gmail.com>
2024-02-12 13:50:17 -0500

    Update coverage_vs_budget processing for easier visualization

--- coverage_vs_budget_processor.py
+++ coverage_vs_budget_processor.py
@@ -1,11 +1,13 @@
 from collections import defaultdict
 
 data = open("coverage_vs_budget.csv", "r").readlines()
-header = data[0]
+header = data[0].strip()
 data = data[1:]
 split_data = []
+algs = set()
 for line in data:
     parts = line.split(',')
+    algs.add(parts[0])
     parts = [parts[0]] + list(map(float, parts[1:]))
     split_data.append(parts)
 
@@ -23,8 +25,24 @@ for key, value in d.items():
         total[i] /= len(value)
     d_averages[key] = total
 
+budgets = set()
+for key in d_averages.keys():
+    budgets.add(key[1])
+budgets = list(budgets)
+budgets.sort()
+
+header_parts = header.split(',')
+new_header_list = [header_parts[1]]
+for a in algs:
+    for data in header_parts[2:]:
+        new_header_list.append(a + " " + data)
+header = ','.join(new_header_list)
+
 with open("coverage_vs_budget_processed.csv", "w") as file:
     file.write(header + '\n')
-    for key, value in d_averages.items():
-        row = list(key) + value
+    for budget in budgets:
+        row = []
+        row.append(budget)
+        for alg in algs:
+            row.extend(d_averages[(alg, budget)])
         file.write(','.join(map(str, row)) + '\n')
